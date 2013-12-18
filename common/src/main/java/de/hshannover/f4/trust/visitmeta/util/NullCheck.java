930bb90439566e0d1c63bba8937ab1dfdd28fa7d
Bastian Hellmann <bahellma@googlemail.com>
2013-12-18 15:29:08 +0100

    Fixed two package header

--- common/src/main/java/de/hshannover/f4/trust/visitmeta/util/NullCheck.java
+++ common/src/main/java/de/hshannover/f4/trust/visitmeta/util/NullCheck.java
@@ -36,34 +36,12 @@
  * limitations under the License.
  * #L%
  */
-/*
- * Project: irond
- * Package: src.de.fhhannover.inform.iron.mapserver.utils
- * File:    NullCheck.java
- *
- * Copyright (C) 2010-2011 Fachhochschule Hannover
- * Ricklinger Stadtweg 118, 30459 Hannover, Germany 
- *
- * Licensed under the Apache License, Version 2.0 (the License);
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an AS IS BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package de.hshannover.f4.trust.visitmeta.util;
 
-
-
-
-
 /**
+ * Copied from project: irond (https://github.com/trustathsh/irond.git
+ * Package: src.de.fhhannover.inform.iron.mapserver.utils
+ *
  * Stupid Helper Class
  * 
  * @author aw

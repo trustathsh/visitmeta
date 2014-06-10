5f51454f0363ef0b145ff29fb8ea9d4586da73de
Thomas Rossow <rosso@parttimegeeks.net>
2014-06-10 12:15:10 +0200

    Version 0.0.5 release

--- common/src/main/java/de/hshannover/f4/trust/visitmeta/interfaces/Identifier.java
+++ common/src/main/java/de/hshannover/f4/trust/visitmeta/interfaces/Identifier.java
@@ -18,7 +18,7 @@
  * Email: trust@f4-i.fh-hannover.de
  * Website: http://trust.f4.hs-hannover.de/
  * 
- * This file is part of visitmeta common, version 0.0.3,
+ * This file is part of visitmeta common, version 0.0.5,
  * implemented by the Trust@HsH research group at the Hochschule Hannover.
  * %%
  * Copyright (C) 2012 - 2013 Trust@HsH

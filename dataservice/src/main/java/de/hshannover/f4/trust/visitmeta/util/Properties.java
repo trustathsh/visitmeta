b1b9c8811c244e4e93577693dc936b8b5934d278
Marcel Reichenbach <MarcelReichenbach@Live.de>
2014-08-28 09:34:02 +0200

    [add] - Java-Doc and exception handling to Properties class

--- dataservice/src/main/java/de/hshannover/f4/trust/visitmeta/util/Properties.java
+++ dataservice/src/main/java/de/hshannover/f4/trust/visitmeta/util/Properties.java
@@ -1,16 +1,14 @@
 package de.hshannover.f4.trust.visitmeta.util;
 
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Map;
-
-import org.apache.log4j.Logger;
 
 import de.hshannover.f4.trust.visitmeta.util.yaml.PropertiesReader;
 import de.hshannover.f4.trust.visitmeta.util.yaml.PropertiesWriter;
+import de.hshannover.f4.trust.visitmeta.util.yaml.PropertyException;
 
 public class Properties {
-
-	private static final Logger log = Logger.getLogger(Properties.class);
 
 	private PropertiesWriter mWriter;
 
@@ -20,7 +18,7 @@ public class Properties {
 
 	private Map<String, Object> mApplicationConfigs;
 
-	private Properties(Properties context) throws FileNotFoundException{
+	private Properties(Properties context) {
 		mWriter = context.mWriter;
 		mReader = context.mReader;
 		mFileName = context.mFileName;
@@ -28,63 +26,154 @@ public class Properties {
 	}
 
 	/**
-	 * Create a ApplicationProperties for load and save Application-YML files.
+	 * Create a ApplicationProperties for load and save Application-YAML files.
 	 *
 	 * @param fileName
 	 * @throws FileNotFoundException
 	 */
 	public Properties(String fileName) throws FileNotFoundException{
-		log.trace("new Properties()...");
+		NullCheck.check(fileName, "fileName is null");
 		mFileName = fileName;
 		mWriter = new PropertiesWriter(mFileName);
 		mReader = new PropertiesReader(mFileName);
 		load();
 	}
 
+	/**
+	 * Load the YAML-files new.
+	 *
+	 * @throws FileNotFoundException
+	 */
 	public void load() throws FileNotFoundException{
 		mApplicationConfigs = mReader.load();
 	}
 
+	/**
+	 * Save all properties in a YAML-files and override them.
+	 *
+	 * @throws FileNotFoundException
+	 */
 	public void save() throws FileNotFoundException {
 		mWriter.persist(mApplicationConfigs);
 	}
 
+	/**
+	 * Get a new Properties from the property key. Only property keys accepted but not a path.
+	 * 
+	 * @param propertyKey
+	 * @throws PropertyException
+	 */
 	@SuppressWarnings("unchecked")
-	public Properties get(String propertyKey) throws FileNotFoundException{
+	public Properties get(String propertyKey) throws PropertyException {
+		NullCheck.check(propertyKey, "propertyKey is null");
 		Properties context = new Properties(this);
-		context.mApplicationConfigs = (Map<String, Object>) mApplicationConfigs.get(propertyKey);
+		Object o = mApplicationConfigs.get(propertyKey);
+		if(o == null){
+			throw new PropertyException(propertyKey + " is not a property key!");
+		}
+		if(o instanceof Map){
+			context.mApplicationConfigs = (Map<String, Object>) o;
+		}else{
+			throw new PropertyException(propertyKey + " is not part of a property path!");
+		}
 		return context;
 	}
 
+	/**
+	 * Get the value from the property path.
+	 * 
+	 * @param propertyPath foo.bar.property
+	 * @return Object
+	 * @throws PropertyException
+	 */
 	@SuppressWarnings("unchecked")
-	public Object getValue(String propertyPath) {
+	public Object getValue(String propertyPath) throws PropertyException {
+		// check propertyPath
+		NullCheck.check(propertyPath, "propertyPath is null");
+
+		// split propertyPath
 		String[] propertyKeyArray = propertyPath.split("\\.");
+
+		// iterate the root Map for every key
 		Map<String, Object> applicationConfigs = mApplicationConfigs;
 		for(int i=0; i<propertyKeyArray.length; i++){
 			Object tmp = applicationConfigs.get(propertyKeyArray[i]);
+			if(tmp == null){
+				if(propertyKeyArray.length > 1){
+					throw new PropertyException("property path[" + propertyPath +"] have not a property key["+ propertyKeyArray[i] + "] !");
+				} else {
+					throw new PropertyException(propertyKeyArray[i] + "] is not a property key!");
+				}
+			}
 			if(tmp instanceof Map){
 				applicationConfigs = (Map<String, Object>) applicationConfigs.get(propertyKeyArray[i]);
-			}else if(tmp instanceof String || tmp instanceof Integer || tmp instanceof Double || tmp instanceof Boolean){
+			}else {
 				return tmp;
 			}
 		}
-		return null;
+		return applicationConfigs;
 	}
 
-	public String getString(String propertyKey) {
-		return (String) getValue(propertyKey);
+	/**
+	 * Get the String-value from the property path. Throw a PropertyException when the value is not a String.
+	 * 
+	 * @param propertyPath foo.bar.property
+	 * @return String
+	 * @throws PropertyException
+	 */
+	public String getString(String propertyPath) throws PropertyException {
+		Object o =  getValue(propertyPath);
+		if(o instanceof String){
+			return (String) o;
+		}
+		throw new PropertyException("The value from property path[" + propertyPath + "] is not a String!");
 	}
 
-	public int getInt(String propertyKey) {
-		return (int) getValue(propertyKey);
+	/**
+	 * Get the int-value from the property path. Throw a PropertyException when the value is not a int.
+	 * 
+	 * @param propertyPath foo.bar.property
+	 * @return String
+	 * @throws PropertyException
+	 */
+	public int getInt(String propertyPath) throws PropertyException {
+		Object o =  getValue(propertyPath);
+		if(o instanceof Integer){
+			return (int) o;
+		}
+		throw new PropertyException("The value from property path[" + propertyPath + "] is not a int!");
 	}
 
-	public double getDouble(String propertyKey) {
-		return (double) getValue(propertyKey);
+	/**
+	 * Get the double-value from the property path. Throw a PropertyException when the value is not a double.
+	 * 
+	 * @param propertyPath foo.bar.property
+	 * @return String
+	 * @throws PropertyException
+	 */
+	public double getDouble(String propertyPath) throws PropertyException {
+		Object o =  getValue(propertyPath);
+		if(o instanceof Double){
+			return (double) o;
+		}
+		throw new PropertyException("The value from property path[" + propertyPath + "] is not a double!");
 	}
 
-	public boolean getBoolean(String propertyKey) {
-		return (boolean) getValue(propertyKey);
+	/**
+	 * Get the boolean-value from the property path. Throw a PropertyException when the value is not a boolean.
+	 * 
+	 * @param propertyPath foo.bar.property
+	 * @return String
+	 * @throws PropertyException
+	 */
+	public boolean getBoolean(String propertyPath) throws PropertyException {
+		Object o =  getValue(propertyPath);
+		if(o instanceof Boolean){
+			return (boolean) o;
+		}
+		throw new PropertyException("The value from property path[" + propertyPath + "] is not a boolean!");
+	}
+
 	/**
 	 * Set and save the value under the key.
 	 * 
@@ -128,6 +217,7 @@ public class Properties {
 			throw new PropertyException("File " + mFileName + " not found!");
 		}
 	}
+
 
 	@Override
 	public String toString(){

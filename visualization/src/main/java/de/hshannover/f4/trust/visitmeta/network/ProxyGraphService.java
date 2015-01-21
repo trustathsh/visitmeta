e57449cc946e0536181a087acaf3fd66b80b8801
Bastian Hellmann <bahellma@googlemail.com>
2015-01-21 16:55:39 +0100

    [fixed] wrongly parsing of ifmap-timestamp-fraction as a XSD date-time

--- visualization/src/main/java/de/hshannover/f4/trust/visitmeta/network/ProxyGraphService.java
+++ visualization/src/main/java/de/hshannover/f4/trust/visitmeta/network/ProxyGraphService.java
@@ -7,17 +7,17 @@
  *    | | | |  | |_| \__ \ |_| | (_| |  _  |\__ \|  _  |
  *    |_| |_|   \__,_|___/\__|\ \__,_|_| |_||___/|_| |_|
  *                             \____/
- * 
+ *
  * =====================================================
- * 
+ *
  * Hochschule Hannover
  * (University of Applied Sciences and Arts, Hannover)
  * Faculty IV, Dept. of Computer Science
  * Ricklinger Stadtweg 118, 30459 Hannover, Germany
- * 
+ *
  * Email: trust@f4-i.fh-hannover.de
  * Website: http://trust.f4.hs-hannover.de/
- * 
+ *
  * This file is part of visitmeta-visualization, version 0.3.0,
  * implemented by the Trust@HsH research group at the Hochschule Hannover.
  * %%
@@ -26,9 +26,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -104,7 +104,7 @@ public class ProxyGraphService implements GraphService {
 		mService = service;
 		mIncludeRawXML = false;
 	}
-	
+
 	public ProxyGraphService(WebResource service, boolean includeRawXML) {
 		mService = service;
 		mIncludeRawXML = includeRawXML;
@@ -148,7 +148,7 @@ public class ProxyGraphService implements GraphService {
 				.queryParam("rawData", Boolean.toString(mIncludeRawXML))
 				.accept(MediaType.APPLICATION_JSON)
 				.get(String.class);
-		
+
 		JsonNode rootNode = parseJson(json);
 		return extractGraphsFromJson(rootNode);
 	}
@@ -161,7 +161,7 @@ public class ProxyGraphService implements GraphService {
 				.queryParam("rawData", Boolean.toString(mIncludeRawXML))
 				.accept(MediaType.APPLICATION_JSON)
 				.get(String.class);
-		
+
 		JsonNode rootNode = parseJson(json);
 		return extractGraphsFromJson(rootNode);
 	}
@@ -212,36 +212,36 @@ public class ProxyGraphService implements GraphService {
 	private Delta extractDeltasFromJson(String json) {
 		List<IdentifierGraph> updates = new ArrayList<IdentifierGraph>();
 		List<IdentifierGraph> deletes = new ArrayList<IdentifierGraph>();
-		
+
 		// TODO fill lists
 		/**
 		 * {"updates":[],"deletes":[]}
 		 * multiple timestamps
 		 */
 		JsonNode rootNode = parseJson(json);
-		
+
 		JsonNode jsonDeltaUpdates = rootNode.get(DELTA_UPDATES);
 		if (jsonDeltaUpdates != null) {
 			log.trace("Found updates on delta");
 			if (jsonDeltaUpdates.isArray()) {
 				Iterator<JsonNode> updateElements = jsonDeltaUpdates.getElements();
-				while (updateElements.hasNext()) {					
+				while (updateElements.hasNext()) {
 					updates.add(buildGraphFromJson(updateElements.next()));
 				}
 			}
 		}
-		
+
 		JsonNode jsonDeltaDeletes = rootNode.get(DELTA_DELETES);
 		if (jsonDeltaUpdates != null) {
 			log.trace("Found deletes on delta");
 			if (jsonDeltaDeletes.isArray()) {
 				Iterator<JsonNode> deleteElements = jsonDeltaDeletes.getElements();
-				while (deleteElements.hasNext()) {					
+				while (deleteElements.hasNext()) {
 					deletes.add(buildGraphFromJson(deleteElements.next()));
 				}
 			}
 		}
-		
+
 		return new DeltaImpl(deletes, updates);
 	}
 
@@ -307,10 +307,10 @@ public class ProxyGraphService implements GraphService {
 				for (Metadata m : metadataList) {
 					linkImpl.addMetadata(m);
 				}
-				
+
 				log.trace("Creating link: " + linkImpl);
-				
-			// one identifier
+
+				// one identifier
 			} else {
 				log.trace("JsonNode contains one identifier");
 
@@ -347,10 +347,10 @@ public class ProxyGraphService implements GraphService {
 			log.trace("No metadata found.");
 			return new ArrayList<Metadata>();
 		}
-		
+
 		Metadata metadatum = null;
 		List<Metadata> metadataList = new ArrayList<Metadata>();
-		
+
 		// multiple metadata
 		if (jsonNode.isArray()) {
 			Iterator<JsonNode> metadatas = jsonNode.getElements();
@@ -360,13 +360,13 @@ public class ProxyGraphService implements GraphService {
 				metadatum = buildMetadataFromJson(currentMetadata);
 				metadataList.add(metadatum);
 			}
-			
-		// only one metadatum
+
+			// only one metadatum
 		} else {
 			metadatum = buildMetadataFromJson(jsonNode);
 			metadataList.add(metadatum);
 		}
-		
+
 		return metadataList;
 	}
 
@@ -378,44 +378,44 @@ public class ProxyGraphService implements GraphService {
 			throw new RuntimeException("no typename found for metadata '"+jsonMetadata+"'");
 		}
 		String typename = jsonTypename.getValueAsText();
-		
+
 		JsonNode jsonProperties = jsonMetadata.get(PROPERTIES);
 		if (jsonProperties == null) {
 			throw new RuntimeException("no properties found for metadata '"+jsonMetadata+"'");
 		}
-		
+
 		boolean isSingleValue = false;
 		long publishTimestamp = 0;
 		Map<String, String> propertiesMap = new HashMap<String, String>();
-		
+
 		Iterator<String> properties = jsonProperties.getFieldNames();
 		while (properties.hasNext()) {
 			String key = properties.next();
 			String value = jsonProperties.get(key).getValueAsText();
-			
+
 			if (key.contains("@ifmap-cardinality")) {
 				isSingleValue = value.equalsIgnoreCase("singleValue") ? true : false;
-			} else if (key.contains("@ifmap-timestamp")) {
+			} else if (key.equals("@ifmap-timestamp")) {
 				publishTimestamp = getXsdStringAsCalendar(value);
 			}
-			
+
 			propertiesMap.put(key, value);
 		}
-		
+
 		log.trace("Creating metadata (typename: " + typename + ", isSingleValue: " + isSingleValue + ", publishTimestamp: " + publishTimestamp + ")");
 		MetadataImpl metadata = new MetadataImpl(typename, isSingleValue, publishTimestamp);
 		for (String key : propertiesMap.keySet()) {
 			log.trace("Adding property to metadata: " + key + " = " + propertiesMap.get(key));
 			metadata.addProperty(key, propertiesMap.get(key));
 		}
-		
+
 		if (mIncludeRawXML) {
 			JsonNode jsonRawXML = jsonMetadata.get(RAW_XML);
 			if (jsonRawXML != null ) {
 				metadata.setRawData(jsonRawXML.getValueAsText());
 			}
 		}
-		
+
 		return metadata;
 	}
 
@@ -440,14 +440,14 @@ public class ProxyGraphService implements GraphService {
 
 			identifier.addProperty(key, value);
 		}
-		
+
 		if (mIncludeRawXML) {
 			JsonNode jsonRawXML = jsonIdentifier.get(RAW_XML);
 			if (jsonRawXML != null ) {
 				identifier.setRawData(jsonRawXML.getValueAsText());
 			}
 		}
-		
+
 		return identifier;
 	}
 
@@ -487,38 +487,38 @@ public class ProxyGraphService implements GraphService {
 		return graphs;
 	}
 
-    /**
-     * Transforms a given xsd:DateTime {@link String} (e.g. 2003-05-31T13:20:05-05:00) to a Java {@link Calendar} object. Uses DatetypeConverter to parse the
-     * {@link String} object.
-     *
-     * @param xsdDateTime - the xsd:DateTime that is to be transformed.
-     * @return a {@link Calendar} object representing the given xsd:DateTime
-     */
-    private Long getXsdStringAsCalendar(String xsdDateTime) {
-        assert xsdDateTime != null && !xsdDateTime.isEmpty();
-        try {
-            if (xsdDateTime.contains("+")) {
-                int idxTz = xsdDateTime.lastIndexOf("+");
-                int idxLastColon = xsdDateTime.lastIndexOf(":");
-                if(idxLastColon < idxTz) {
-                    String p1 = xsdDateTime.substring(0, idxTz+3);
-                    String p2 = xsdDateTime.substring(idxTz+3, xsdDateTime.length());
-                    xsdDateTime = p1 + ":" + p2;
-                }
-            } 
-            if (xsdDateTime.contains(":")) {	// if the String contains an ':' literal, we try to interpret it as a xsdDateTime-string
-                return DatatypeConverter.parseDateTime(xsdDateTime).getTimeInMillis();
-            } else {	// try to parse a time in milliseconds to a Calendar object
-                Calendar tmp = new GregorianCalendar();
-                tmp.setTimeInMillis(Long.parseLong(xsdDateTime));
-                return tmp.getTimeInMillis();
-            }
-        } catch (IllegalArgumentException e) {
-            log.error("Illegal data/time format found (incoming String was: "
-                    + xsdDateTime + "); setting to current date/time.");
-            return new GregorianCalendar().getTimeInMillis();
-        }
-    }
+	/**
+	 * Transforms a given xsd:DateTime {@link String} (e.g. 2003-05-31T13:20:05-05:00) to a Java {@link Calendar} object. Uses DatetypeConverter to parse the
+	 * {@link String} object.
+	 *
+	 * @param xsdDateTime - the xsd:DateTime that is to be transformed.
+	 * @return a {@link Calendar} object representing the given xsd:DateTime
+	 */
+	private Long getXsdStringAsCalendar(String xsdDateTime) {
+		assert xsdDateTime != null && !xsdDateTime.isEmpty();
+		try {
+			if (xsdDateTime.contains("+")) {
+				int idxTz = xsdDateTime.lastIndexOf("+");
+				int idxLastColon = xsdDateTime.lastIndexOf(":");
+				if(idxLastColon < idxTz) {
+					String p1 = xsdDateTime.substring(0, idxTz+3);
+					String p2 = xsdDateTime.substring(idxTz+3, xsdDateTime.length());
+					xsdDateTime = p1 + ":" + p2;
+				}
+			}
+			if (xsdDateTime.contains(":")) {	// if the String contains an ':' literal, we try to interpret it as a xsdDateTime-string
+				return DatatypeConverter.parseDateTime(xsdDateTime).getTimeInMillis();
+			} else {	// try to parse a time in milliseconds to a Calendar object
+				Calendar tmp = new GregorianCalendar();
+				tmp.setTimeInMillis(Long.parseLong(xsdDateTime));
+				return tmp.getTimeInMillis();
+			}
+		} catch (IllegalArgumentException e) {
+			log.error("Illegal data/time format found (incoming String was: "
+					+ xsdDateTime + "); setting to current date/time.");
+			return new GregorianCalendar().getTimeInMillis();
+		}
+	}
 
 	@Override
 	public long count(GraphType type) {
@@ -549,5 +549,5 @@ public class ProxyGraphService implements GraphService {
 		log.error("Not implemented!");
 		throw new RuntimeException("Not implemented!");
 	}
-	
+
 }

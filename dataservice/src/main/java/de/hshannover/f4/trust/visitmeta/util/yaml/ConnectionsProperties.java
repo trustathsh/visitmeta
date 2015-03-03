64adfae5bf6f4fcc1f90b01d798a6bc1f26dde98
MReichenbach <MarcelReichenbach@Live.de>
2015-03-03 23:19:18 +0100

    [fixed] dataservice - only the default subscription was loaded, now loads all subscriptions for a connection

--- dataservice/src/main/java/de/hshannover/f4/trust/visitmeta/util/yaml/ConnectionsProperties.java
+++ dataservice/src/main/java/de/hshannover/f4/trust/visitmeta/util/yaml/ConnectionsProperties.java
@@ -128,17 +128,12 @@ public class ConnectionsProperties extends Properties {
 	private List<Subscription> buildSubscribtion(String connectionName)
 			throws PropertyException {
 		// try to read SubscribeList from Connection, if not exists return null
-		Properties propertySubscribeList;
-		try {
-			propertySubscribeList = getPropertySubscribeList(connectionName);
-		} catch (PropertyException e) {
-			return null;
-		}
+        Map<String,String> propertySubscribeList = (Map<String, String>) super.get(connectionName).getValue(ConnectionKey.SUBSCRIPTIONS);
 
 		List<Subscription> subscribtionList = new ArrayList<Subscription>();
 
 		// for all Subscriptions
-		for (String subscribeName : propertySubscribeList.getKeySet()) {
+		for (String subscribeName : propertySubscribeList.keySet()) {
 			// build new Subscription
 			Subscription subscribtion = new SubscriptionImpl();
 			// set required values

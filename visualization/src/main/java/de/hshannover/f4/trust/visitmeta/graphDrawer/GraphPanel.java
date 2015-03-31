cde274e94d3bd39e1bf08243248c37de46a10840
Bastian Hellmann <bahellma@googlemail.com>
2015-03-31 09:26:05 +0200

    [fixed] errors with coloring of metadata nodes

--- visualization/src/main/java/de/hshannover/f4/trust/visitmeta/graphDrawer/GraphPanel.java
+++ visualization/src/main/java/de/hshannover/f4/trust/visitmeta/graphDrawer/GraphPanel.java
@@ -7,17 +7,17 @@
  *    | | | |  | |_| \__ \ |_| | (_| |  _  |\__ \|  _  |
  *    |_| |_|   \__,_|___/\__|\ \__,_|_| |_||___/|_| |_|
  *                             \____/
- * 
+ *
  * =====================================================
- * 
+ *
  * Hochschule Hannover
  * (University of Applied Sciences and Arts, Hannover)
  * Faculty IV, Dept. of Computer Science
  * Ricklinger Stadtweg 118, 30459 Hannover, Germany
- * 
+ *
  * Email: trust@f4-i.fh-hannover.de
  * Website: http://trust.f4.hs-hannover.de/
- * 
+ *
  * This file is part of visitmeta-visualization, version 0.4.1,
  * implemented by the Trust@HsH research group at the Hochschule Hannover.
  * %%
@@ -26,9 +26,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -122,10 +122,9 @@ public interface GraphPanel {
 	public void adjustPanelSize();
 	/**
 	 * Repaint nodes of a specific type and publisher.
-	 * @param pPublisher the id of the current publisher, empty if the default color is changed.
 	 * @param pType the type of the node,
 	 */
-	public void repaintNodes(NodeType pType, String pPublisher);
+	public void repaintNodes(NodeType pType);
 	/**
 	 * Repaint the panel.
 	 */
@@ -142,7 +141,7 @@ public interface GraphPanel {
 
 	/**
 	 * Mark a node as selected.
-	 * 
+	 *
 	 * @param propable the {@link Propable} object that was selected
 	 */
 	public void selectNode(Propable propable);

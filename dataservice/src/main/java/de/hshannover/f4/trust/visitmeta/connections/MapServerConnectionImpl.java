ea901dfa63e01ef449933702875a104fc4caf649
MReichenbach <MarcelReichenbach@Live.de>
2015-06-24 21:13:39 +0200

    [fixed] dataservice MapServerConnectionImpl constructor

--- dataservice/src/main/java/de/hshannover/f4/trust/visitmeta/connections/MapServerConnectionImpl.java
+++ dataservice/src/main/java/de/hshannover/f4/trust/visitmeta/connections/MapServerConnectionImpl.java
@@ -20,9 +20,11 @@ import de.hshannover.f4.trust.visitmeta.exceptions.ifmap.ConnectionException;
 import de.hshannover.f4.trust.visitmeta.exceptions.ifmap.IfmapConnectionException;
 import de.hshannover.f4.trust.visitmeta.exceptions.ifmap.NotConnectedException;
 import de.hshannover.f4.trust.visitmeta.ifmap.SubscriptionHelper;
+import de.hshannover.f4.trust.visitmeta.ifmap.SubscriptionImpl;
 import de.hshannover.f4.trust.visitmeta.ifmap.UpdateService;
 import de.hshannover.f4.trust.visitmeta.interfaces.GraphService;
 import de.hshannover.f4.trust.visitmeta.interfaces.Subscription;
+import de.hshannover.f4.trust.visitmeta.interfaces.SubscriptionData;
 import de.hshannover.f4.trust.visitmeta.interfaces.connections.MapServerConnection;
 import de.hshannover.f4.trust.visitmeta.interfaces.connections.MapServerConnectionData;
 import de.hshannover.f4.trust.visitmeta.interfaces.data.Data;
@@ -85,7 +87,13 @@ public class MapServerConnectionImpl extends MapServerConnectionDataImpl impleme
 
 		super.setStartupConnect(connectionData.doesConnectOnStartup());
 		super.setAuthenticationBasic(connectionData.isAuthenticationBasic());
-		super.setSubscriptionData(connectionData.getSubscriptions());
+
+		for (Data subscriptonData : connectionData.getSubscriptions()) {
+			if (subscriptonData instanceof SubscriptionData) {
+				super.addSubscription(new SubscriptionImpl(this, (SubscriptionData) subscriptonData));
+			}
+		}
+
 		super.setConnected(connectionData.isConnected());
 	}
 
